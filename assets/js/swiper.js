585c28e879cbc063afc532f45119562964b2ac7f
jhdpc <as313234@gmail.com>
2024-09-08 23:28:32 +0800

    首頁開發(search)

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -2,39 +2,77 @@
 // 匯入全部模組
 import Swiper from "swiper/bundle";
 
-// test
 
-const swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1, // 一次呈現幾張
+const featureSwiper = new Swiper(".featureSwiper", {
+  slidesPerView: 2, // 一次呈現幾張
   slidesPerGroup: 1, // 一個群組內有幾張投影片
-  spaceBetween: 24, // 每個內容之間的間隔為 24px
+  spaceBetween: 16, // 每個內容之間的間隔為 24px
   //間隔的設置通過 JavaScript 傳遞到數據庫，然後返回並應用，而不是通過本地 CSS 設置。
 
   loop: true, // 循環播放
 
   // 分頁展示方式
   pagination: {
-    el: ".swiper-pagination",
+    el: ".feature-Swiperr-pagination",
     type: "fraction", // 分數
   },
 
   // 左右箭頭 功能設定
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".feature-Swiper-button-next",
+    prevEl: ".feature-Swiper-button-prev",
   },
 
   //RWD
   breakpoints: {
-    767: {
-      slidesPerView: 2, // 一次呈現幾張
-      slidesPerGroup: 1, // 一個群組內有幾張投影片
-      spaceBetween: 48, // 每個內容之間的間隔為 48px
+    992: {
+      slidesPerView: 4, // 一次呈現幾張
+      slidesPerGroup: 4, // 一個群組內有幾張投影片
+      spaceBetween: 24, // 每個內容之間的間隔為 48px
+      loop: false,
     },
   },
 });
 
-// test2
+
+const searchSwiper = new Swiper(".searchSwiper", {
+  slidesPerView: 2, // 一次呈現幾張
+  slidesPerGroup: 1, // 一個群組內有幾張投影片
+  spaceBetween: 8, // 每個內容之間的間隔為 24px
+  //間隔的設置通過 JavaScript 傳遞到數據庫，然後返回並應用，而不是通過本地 CSS 設置。
+
+  loop: false, // 循環播放
+
+  // 分頁展示方式
+  pagination: {
+    el: ".searchSwiper-pagination",
+    type: "fraction", // 分數
+  },
+
+  // 左右箭頭 功能設定
+  navigation: {
+    nextEl: ".searchSwiper-button-next",
+    prevEl: ".searchSwiper-button-prev",
+  },
+
+  //RWD
+  breakpoints: {
+    992: {
+      slidesPerView: 6, // 一次呈現幾張
+      slidesPerGroup: 1, // 一個群組內有幾張投影片
+      spaceBetween: 0, // 每個內容之間的間隔為 48px
+      loop: false,
+    },
+    1200: {
+      slidesPerView: 9, // 一次呈現幾張
+      slidesPerGroup: 9, // 一個群組內有幾張投影片
+      spaceBetween: 0, // 每個內容之間的間隔為 48px
+      loop: false,
+    },
+  },
+});
+
+// test
 
 const mySwiper2 = new Swiper('.mySwiper2', {
     // 循環播放
